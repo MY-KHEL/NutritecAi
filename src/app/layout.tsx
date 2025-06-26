15ed73efbd12abc503c553df1fade45f1c0e17e4
Michael <ojomichael965@gmail.com>
2025-06-26 18:25:55 +0100

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
       >
         <div className="relative">
           {children}
-          <div className="absolute w-full h-full backdrop-blur-sm bg-black/30 top-0 left-0">
-          <div className="p-16 rounded-2xl bg-black/50 w-full md:w-3/4  h-fit mt-20 md:mt-40 m-auto text-center">
+          <div className="absolute w-full h-full backdrop-blur-sm p-6 bg-black/30 top-0 left-0">
+          <div className="p-16 rounded-2xl bg-black/50  w-full md:w-3/4  h-fit mt-20 md:mt-40 m-auto text-center">
             <h1 className="text-4xl md:text-6xl font-sans text-white"> <span className="text-project-green">NutritecAi</span> is currently in its  <span className="text-project-pink">Development</span> stage and will be launched  <span className="text-project-green">soon.</span></h1>
           </div>
           </div>
